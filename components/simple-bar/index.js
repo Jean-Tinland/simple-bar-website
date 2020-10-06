c659466f341eee7378a3d618c2e1355889215558
Jean <jean.tinland@gmail.com>
2020-10-06 18:56:26 +0200

    WIP: fake simple bar component

---
+++ components/simple-bar/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const SimpleBar = () => {
+  return (
+    <div className="simple-bar">
+      <div className="simple-bar__spaces"></div>
+      <div className="simple-bar__process"></div>
+      <div className="simple-bar__data"></div>
+    </div>
+  )
+}
+
+export default SimpleBar
--- data.js
+++ data.js
@@ -12,3 +12,33 @@ export const locales = {
     'introduction-tagline': 'Un widget pour <b>yabai</b> & <b>Übersicht</b>'
   }
 }
+
+export const fakeBar = {
+  spaces: 3,
+  process: [
+    {
+      app: 'Code',
+      space: 1
+    },
+    {
+      app: 'Chrome',
+      space: 2
+    },
+    {
+      app: 'Skype',
+      space: 3
+    },
+    {
+      app: 'Slack',
+      space: 3
+    }
+  ],
+  data: {
+    battery: 100,
+    sound: 33,
+    wifi: {
+      fr: 'My wifi',
+      en: 'Mon wifi'
+    }
+  }
+}
